7a50b932bc973cc99214116dfcc1831468b9d3c9
Muhammed Zenhal Abdulla <muhammedzenhal3@gmail.com>
2026-08-17 20:40:22 +0530

    New Commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        contact: 'contact.html',
+        gallery: 'gallery.html',
+        process: 'process.html',
+        projects: 'projects.html',
+        services: 'services.html',
+        testimonials: 'testimonials.html',
+      },
+    },
+  },
 });
